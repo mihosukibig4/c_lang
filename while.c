aaab569c105924d950d8a2620eb884cc9d0ebe58
Takeshi Okinawa <mihosukibig4@gmail.com>
2018-04-02 16:00:26 +0900

    add do_while

--- while.c
+++ while.c
@@ -3,11 +3,19 @@
 /* roop  */
 
 int main(void){
+
     int m = 0;
     while (m<11){
       printf("m: %d\n", m);
       m++;
 
     }
+
+		int n = 0;
+		do{
+			printf("n: %d\n", n);
+			n++;
+		} while (n < 11);
+		
     return 0;
 }
